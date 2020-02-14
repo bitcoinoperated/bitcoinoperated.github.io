e7f172eaecc9b0f9556d43460e127d5754b98e48
bitcoinoperated <mr.peter.april@gmail.com>
2020-02-14 14:54:14 +0100

    Update Bloody_Hours.js

--- Bloody_Hours.js
+++ Bloody_Hours.js
@@ -9,10 +9,13 @@ if ( reply >= 6 && reply <= 12 ) {
 window.alert ('Good Morning')
 }
 else if ( reply > 12 && reply <= 18 ) {
-window.alert ('Good Afternoon')
+	window.alert ('Good Afternoon')
+}
+else if ( reply > 18 && reply <= 24 ) {
+window.alert ('Good Evening')
 }
 else
-window.alert ('Good Evening');
+window.alert ('Good Night');
 }
 greeting(prompt("Hello. What time is it?"));
   
